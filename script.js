bcd2a998b18315cad86671910a76610fbd6ef136
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-01-29 23:18:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+function myFunc() {
+	let name = document.getElementById("name").innerText;
+	let year = document.getElementById("year").innerText;
+
+	if(name != "" && year != "") {
+		let newUrl = document.getElementById("url").innerText;
+		newUrl.innerText = newUrl + `?name=${name}&${year}`;
+	}
+	else if(name != "") {
+		let newUrl = document.getElementById("url").innerText;
+		newUrl.innerText = newUrl + `?name=${name}`;
+	}
+	else {
+		let newUrl = document.getElementById("url").innerText;
+		newUrl.innerText = newUrl + `?${year}`;
+	}
+}
