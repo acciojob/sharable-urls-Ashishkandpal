1be6ef522929fc26e9feb7335f7884fa34182ed4
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-01-30 13:15:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 let name = document.getElementById("name").value;
 let year = document.getElementById("year").value;
 function myFunc() {
-	let prev = document.getElementById("url").innerText;
-	if(name != "" && year != "") {
-		let newUrl = document.getElementById("url");
-		newUrl.innerText = prev + `?name=${name}&${year}`;
-	}
-	else if(name != "") {
-		let newUrl = document.getElementById("url");
-		newUrl.innerText = prev + `?name=${name}`;
-	}
-	else {
-		let newUrl = document.getElementById("url");
-		newUrl.innerText = prev + `?${year}`;
-	}
+	let prev = document.getElementById("url").innerText = "java is java";
+	// if(name != "" && year != "") {
+	// 	let newUrl = document.getElementById("url");
+	// 	newUrl.innerText = prev + `?name=${name}&${year}`;
+	// }
+	// else if(name != "") {
+	// 	let newUrl = document.getElementById("url");
+	// 	newUrl.innerText = prev + `?name=${name}`;
+	// }
+	// else {
+	// 	let newUrl = document.getElementById("url");
+	// 	newUrl.innerText = prev + `?${year}`;
+	// }
 }
